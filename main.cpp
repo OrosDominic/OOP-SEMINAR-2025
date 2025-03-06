e1f576932276af82bf2dfb126d906b37ab4eaf5b
Oros Dominic <dominicoros859@yahoo.com>
2025-03-06 19:57:26 +0200

    am bagat main frate

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
-int main()
-    {
-  return 2;
+#include <iostream>
+
+class Caine {
+    public:
+    bool manca() {
+        std::cout<<"Caine a mancat";
+        return true;
+    }
+    int setAge(int newage) {
+        age=newage;
+        return age;
+    }
+    int getAge() {
+        return age;
+    }
+    private:
+    int age;
+};
+int main(){
+    Caine c=Caine();
+    c.manca();
+    c.setAge(10);
+    std::cout<<c.getAge();
+
+  return 0;
     }
 
 
